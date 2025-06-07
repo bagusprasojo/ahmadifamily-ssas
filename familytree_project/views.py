437cc368914198be9d187088c267b0d4ce9b3d44
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2025-06-07 12:43:30 +0700

    tambah fitur ultah

--- familytree_project/views.py
+++ familytree_project/views.py
@@ -3,10 +3,16 @@ from eventapp.models import Event
 from galleryapp.models import Gallery, GalleryImage 
 from eventapp.models import  Event 
 from treeapp.models import Person
+from datetime import datetime
 
 def home(request):
     latest_event = Event.objects.order_by('-created_at').prefetch_related('images').first()
     recent_photos = GalleryImage.objects.order_by('-id')[:10]
+
+    bulan_sekarang = datetime.now().month
+    ulang_tahun_bulan_ini = Person.objects.filter(
+        birth_date__month=bulan_sekarang
+    ).order_by('birth_date')
 
     jumlah_laki_laki = Person.objects.filter(gender='M').count()
     jumlah_perempuan = Person.objects.filter(gender='F').count()
@@ -22,6 +28,7 @@ def home(request):
         'total_keluarga': total_keluarga,
         'keluarga_terakhir': keluarga_terakhir,
         'recent_photos': recent_photos,
+        'ulang_tahun_bulan_ini': ulang_tahun_bulan_ini,
     }
 
     return render(request, 'home.html', context)
