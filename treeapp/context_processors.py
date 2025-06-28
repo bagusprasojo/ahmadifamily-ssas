348ad3ea5f275d29f00595b4bfc1155a324f1781
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2025-06-28 14:45:09 +0700

    generate ulang req.txt

--- treeapp/context_processors.py
+++ treeapp/context_processors.py
@@ -5,8 +5,9 @@ def root_person(request):
     pasangan_root = None
 
     root = Person.objects.filter(is_root=True).first()
-    if root:
-        pasangan_root = Marriage.objects.filter(husband=root).first().wife
+    if root : 
+        if Person.objects.all().count() > 2:
+            pasangan_root = Marriage.objects.filter(husband=root).first().wife
         
 
     return {
